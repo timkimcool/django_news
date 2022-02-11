f030a97d884f1010c8f56872f40d36637b94ecd5
timkimcool <timkimcool@gmail.com>
2022-02-11 14:02:15 -0500

    update config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ TIME_ZONE = 'America/New_York'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
